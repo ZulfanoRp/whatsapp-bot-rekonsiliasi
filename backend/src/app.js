d41c5600eace3618a4f623ad9206b355a00cba25
ZulfanoRp <zulfano67@gmail.com>
2025-12-21 17:55:59 +0700

    feat: setup backend and mysql connection

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,22 @@
+require('dotenv').config(); // WAJIB
+
 const express = require('express');
 const app = express();
+const pool = require('./config/db');
 
 app.use(express.json());
 
 app.get('/health', (req, res) => {
   res.json({ status: 'OK' });
+});
+
+app.get('/db-test', async (req, res) => {
+  try {
+    const [rows] = await pool.query('SELECT 1 AS test');
+    res.json({ db: 'connected', result: rows });
+  } catch (error) {
+    res.status(500).json({ db: 'error', error: error.message });
+  }
 });
 
 const PORT = process.env.PORT || 3000;
